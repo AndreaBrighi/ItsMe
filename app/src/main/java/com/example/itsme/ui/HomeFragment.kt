54193e8af773214495b33b37b624d1f7cf43f885
Andrea Brighi <abrighi@bsdsoftware.it>
2021-05-07 11:41:42 +0200

    Change theme colors

--- app/src/main/java/com/example/itsme/ui/HomeFragment.kt
+++ app/src/main/java/com/example/itsme/ui/HomeFragment.kt
@@ -1,4 +1,4 @@
-package com.example.itsme
+package com.example.itsme.ui
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
+import com.example.itsme.XMLManager
 import com.example.itsme.databinding.FragmentHomeBinding
 import com.example.itsme.ui.main.SectionsPagerAdapter
 import com.google.android.material.tabs.TabLayout
