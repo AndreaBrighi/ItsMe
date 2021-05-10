fd95784ba9b3c328d33454d6fbe451d5d7436c7d
Andrea Brighi <abrighi@bsdsoftware.it>
2021-05-10 12:53:10 +0200

    Add share file

--- app/src/main/java/com/example/itsme/recyclerview/CardViewHolder.kt
+++ app/src/main/java/com/example/itsme/recyclerview/CardViewHolder.kt
@@ -1,6 +1,13 @@
 package com.example.itsme.recyclerview
 
+import android.content.ContentUris
+import android.content.ContentValues
 import android.content.Intent
+import android.database.Cursor
+import android.net.Uri
+import android.os.Environment
+import android.provider.MediaStore
+import android.widget.Toast
 import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.commit
 import androidx.recyclerview.widget.RecyclerView
@@ -8,6 +15,8 @@ import com.example.itsme.DetailsFragment
 import com.example.itsme.FindActivity
 import com.example.itsme.R
 import com.example.itsme.databinding.CardPreviewBinding
+import java.io.*
+
 
 class CardViewHolder(
     private val itemBinding: CardPreviewBinding,
@@ -25,7 +34,7 @@ class CardViewHolder(
             }
         }
 
-        itemBinding.card.setOnClickListener{
+        itemBinding.card.setOnClickListener {
             activity.supportFragmentManager.commit {
                 setReorderingAllowed(true)
                 replace(R.id.fragment_container_view, DetailsFragment(), null)
@@ -40,8 +49,95 @@ class CardViewHolder(
         }
 
         itemBinding.shareButton.setOnClickListener {
+            val uriText: Uri? = saveFile()
+            if (uriText != null) {
 
+                val shareIntent: Intent = Intent().apply {
+                    action = Intent.ACTION_SEND
+                    putExtra(Intent.EXTRA_STREAM, uriText)
+                    type = "text/cxml"
+                }
+                activity.startActivity(Intent.createChooser(shareIntent, "Send"))
+            }
         }
     }
 
+    private fun saveFile(): Uri? {
+
+        val name = "card.cxml"
+
+
+        val contentUri = MediaStore.Files.getContentUri("external")
+
+        val selection = MediaStore.MediaColumns.RELATIVE_PATH + "=?"
+
+        val selectionArgs =
+            arrayOf(Environment.DIRECTORY_DOCUMENTS + "/") //must include "/" in front and end
+
+
+        val cursor: Cursor? =
+            activity.contentResolver.query(contentUri, null, selection, selectionArgs, null)
+
+        var uri: Uri? = null
+
+        if (cursor != null) {
+            if (cursor.count != 0) {
+
+                while (cursor.moveToNext()) {
+                    val fileName =
+                        cursor.getString(cursor.getColumnIndex(MediaStore.MediaColumns.DISPLAY_NAME))
+                    if (fileName == name) {                          //must include extension
+                        val id =
+                            cursor.getLong(cursor.getColumnIndex(MediaStore.MediaColumns._ID))
+                        uri = ContentUris.withAppendedId(contentUri, id)
+                        break
+                    }
+                }
+            }
+            try {
+                if (uri == null) {
+                    val values = ContentValues()
+
+                    values.put(
+                        MediaStore.MediaColumns.DISPLAY_NAME,
+                        name
+                    )       //file name
+                    values.put(
+                        MediaStore.MediaColumns.MIME_TYPE,
+                        "text/cxml"
+                    )        //file extension, will automatically add to file
+                    values.put(
+                        MediaStore.MediaColumns.RELATIVE_PATH,
+                        Environment.DIRECTORY_DOCUMENTS + "/"
+                    )     //end "/" is not mandatory
+
+                    uri = activity.contentResolver.insert(
+                        MediaStore.Files.getContentUri("external"),
+                        values
+                    )      //important!
+                }
+                val outputStream: OutputStream? = uri.let {
+                    activity.contentResolver.openOutputStream(it!!, "rwt")
+                }
+
+                val out: Writer = BufferedWriter(OutputStreamWriter(outputStream))
+                out.write(
+                    "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                            "<note>test</note>"
+                )
+                out.close()
+
+                outputStream?.close()
+            } catch (e: IOException) {
+                Toast.makeText(activity, "Fail to write file", Toast.LENGTH_SHORT)
+                    .show()
+
+            }
+
+            cursor.close()
+        }
+
+        return uri
+    }
 }
+
